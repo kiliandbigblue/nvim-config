70fae20adef56dafff7b54306407af501f7baea6
kiliandbigblue <kilian.demeulemeester@bigblue.io>
2024-05-19 20:20:24 +0200

    feature/lazy

---
+++ lua/kiliand/core/init.lua
@@ -0,0 +1,3 @@
+require("kiliand.core.options")
+require("kiliand.core.keymaps")
+
---
+++ lua/kiliand/core/keymaps.lua
@@ -0,0 +1,39 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Move line arounds in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]]) -- delete without yanking
+
+-- This is going to get me cancelled
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+vim.keymap.set("n", "<leader>h", "<C-W>h")
+vim.keymap.set("n", "<leader>j", "<C-W>j")
+vim.keymap.set("n", "<leader>k", "<C-W>k")
+vim.keymap.set("n", "<leader>l", "<C-W>l")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
+
