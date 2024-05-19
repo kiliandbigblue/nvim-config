59f1461cc283e65e22e00b7777cc4bf237e9ac94
kiliandbigblue <kilian.demeulemeester@bigblue.io>
2024-05-19 16:15:14 +0200

    feature/lazy

--- after/plugin/telescope.lua
+++
@@ -1,8 +0,0 @@
-local builtin = require('telescope.builtin')
-local vim = vim
-
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fd', function()
-    builtin.find_files({ search_dirs = { vim.fn.expand('%:p:h') } })
-end)
-vim.keymap.set('n', '<leader>ff', builtin.live_grep, {})
--- lua/kiliand/init.lua
+++
@@ -1,3 +0,0 @@
-require("kiliand.remap")
-require("kiliand.set")
-
