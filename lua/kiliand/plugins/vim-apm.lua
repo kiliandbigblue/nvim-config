a647326bdf107a02f7f8da39457fb55f7938fb29
kiliandbigblue <kilian.demeulemeester@bigblue.io>
2024-05-20 17:27:39 +0200

    remove apm

--- lua/kiliand/plugins/vim-apm.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "theprimeagen/vim-apm",
-    config = function()
-        local apm = require("vim-apm")
-        vim.keymap.set("n", "<leader>apm", function() apm:toggle_monitor() end)
-    end,
-}
