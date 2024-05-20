32e109bb9236fdee5c9f9dd061cadc109ffbf265
kiliandbigblue <kilian.demeulemeester@bigblue.io>
2024-05-20 17:14:48 +0200

    lualine better status

--- lua/kiliand/lazy.lua
+++ lua/kiliand/lazy.lua
@@ -11,6 +11,13 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("kiliand.plugins")
-
+require("lazy").setup("kiliand.plugins", {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+})
 
