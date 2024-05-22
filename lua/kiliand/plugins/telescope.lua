4588a998a82fca6915315a1bfd1099838e53b827
kiliandbigblue <kilian.demeulemeester@bigblue.io>
2024-05-22 14:41:34 +0200

    feature/lazy

--- lua/kiliand/plugins/telescope.lua
+++ lua/kiliand/plugins/telescope.lua
@@ -32,6 +32,7 @@ return {
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+    keymap.set("n", "<leader>fw", "<cmd>Telescope lsp_document_symbols<cr>", { desc = "List all documents symbols" })
   end,
 }
 
