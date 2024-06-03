4c3ee9761754c2d48bbe4372c874b8aba1e30d63
kiliandbigblue <kilian.demeulemeester@bigblue.io>
2024-06-03 15:02:16 +0200

    feature/lazy

--- lua/kiliand/plugins/lsp.lua
+++ lua/kiliand/plugins/lsp.lua
@@ -90,8 +90,9 @@ return {
 					-- TODO: Could include cmd, but not sure those were all relevant flags.
 					--    looks like something i would have added while i was floundering
 					init_options = { clangdFileStatus = true },
-					filetypes = { "c" },
+					filetypes = { "c", "proto", "cpp" },
 				},
+
 				eslint = true,
 			}
 
